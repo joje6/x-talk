ab28670c7f76130cd35ce542d1d9d4f76afaeeb8
joje <joje.attrs@gmail.com>
2018-08-02 17:00:46 +0900

    ..

--- .storybook/stories/index.js
+++ .storybook/stories/index.js
@@ -1,16 +1,14 @@
 import { storiesOf } from '@storybook/vue';
 //import { linkTo } from '@storybook/addon-links';
-import {
-  PageChannel,
-  PageChannels,
-  PageSignin,
-  XButton,
-  XMedia,
-  XMessageInput,
-  XMessageList,
-  XSignIn,
-  XTitlebar
-} from '@/src/components';
+
+import XChannel from '@/src/components/x-channel/x-channel.vue';
+import XChannels from '@/src/components/x-channels/x-channels.vue';
+import XSignin from '@/src/components/x-signin/x-signin.vue';
+import XButton from '@/src/components/x-button/x-button.vue';
+import XMedia from '@/src/components/x-media/x-media.vue';
+import XMessageInput from '@/src/components/x-message-input/x-message-input.vue';
+import XMessageList from '@/src/components/x-message-list/x-message-list.vue';
+import XTitlebar from '@/src/components/x-titlebar/x-titlebar.vue';
 import connect from 'x-talk-connect';
 
 
@@ -29,22 +27,20 @@ export const stories = async () => {
   const channelid = channelinfo.id;
 
   return () => {
-    storiesOf('페이지', module)
+    storiesOf('컴포넌트', module)
       .add('로그인', () => ({
-        components: { PageSignin },
-        template: '<page-signin />',
+        components: { XSignin },
+        template: '<x-signin />',
         //methods: { action: linkTo('Button') }
       }))
       .add('채널목록', () => ({
-        components: { PageChannels },
-        template: '<page-channels @select="" />'
+        components: { XChannels },
+        template: '<x-channels @select="" />'
       }))
       .add('채팅창', () => ({
-        components: { PageChannel },
-        template: `<page-channel channelid="${channelid}" />`,
-      }));
-
-    storiesOf('컴포넌트', module)
+        components: { XChannel },
+        template: `<x-channel data-channelid="${channelid}" />`,
+      }))
       .add('버튼', () => ({
         components: { XButton },
         template: '<x-button>\
@@ -70,10 +66,6 @@ export const stories = async () => {
           우측\
         </div>\
         </x-titlebar>'
-      }))
-      .add('로그인', () => ({
-        components: { XSignIn },
-        template: '<x-sign-in />'
       }))
       .add('메시지입력기', () => ({
         components: { XMessageInput },
